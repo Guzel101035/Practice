9d36d94150d9e61e013a69e28284cac5f9c77920
USER <cuxdi@yandex.ru>
2022-08-02 20:56:52 +0400

    add ex004.py

---
+++ ex004.py
@@ -0,0 +1,9 @@
+count = int(input( "n = "))
+index = 0
+while index < count:
+    if (index % 10) % 4 = 0:
+        print(index,end=" ")
+        index += 1
+
+
+        Показать числа , последняя цифра которого делится на 4
